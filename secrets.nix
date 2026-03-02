c3d43c2d3d599fbd8fb3b4b231db742900ce62c4
Yu-Min Peng <leporuid@icloud.com>
2026-03-02 22:02:58 +0800

    Refactor public key definitions for users

--- secrets.nix
+++ secrets.nix
@@ -1,31 +1,9 @@
 let
-  system-homeserver1 = builtins.readFile ./hosts/homeserver1/id_ed25519.pub;
-
-  robert-macmini = builtins.readFile ./hosts/macmini/users/robert/id_ed25519.pub;
-  robert-macbook-air = builtins.readFile ./hosts/macbook-air/users/robert/id_ed25519.pub;
-  robert-pc3 = builtins.readFile ./hosts/pc3/users/robert/id_ed25519.pub;
-  robert-personal = [
-    robert-pc3
-    robert-macmini
-    robert-macbook-air
+  MacBook-Pro = builtins.readFile ./hosts/MacBook-Pro/id_ed25519.pub;
+  leporuid-MacBook-Pro = builtins.readFile ./hosts/MacBook-Pro/users/leporuid/id_ed25519.pub;
+  leporuid = [ leporuid-MacBook-Pro
   ];
-
-  robert-work-macbookpro = builtins.readFile ./hosts/work-macbookpro/users/robert/id_ed25519.pub;
-  work-pc3 = builtins.readFile ./hosts/pc3/users/work/id_ed25519.pub;
-  robert-work = [
-    robert-work-macbookpro
-    work-pc3
-  ];
-
-  robert = robert-personal ++ robert-work;
 in
 {
-  "hosts/homeserver1/tailscale-homeserver1.age".publicKeys = [ system-homeserver1 ] ++ robert;
-  "hosts/homeserver1/clouddns-config.json.age".publicKeys = [ system-homeserver1 ] ++ robert;
-  "hosts/work-macbookpro/users/robert/work-gitconfig.age".publicKeys = [
-    robert-work-macbookpro
-  ]
-  ++ robert;
-  "hosts/pc3/users/work/gitconfig.age".publicKeys = robert;
-  "config/niri/private.kdl".publicKeys = robert;
+  "hosts/MacBook-Pro/users/leporuid/tailscale-authkey.age".publicKeys = [ MacBook-Pro ] ++ leporuid;
 }
