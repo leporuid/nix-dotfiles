0c6347866830eb406e56a3a8b8efd9510b623143
Yu-Min Peng <leporuid@icloud.com>
2026-03-21 01:27:18 +0800

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-swift.url = "github:reckenrode/nixpkgs?ref=swift-update-mk2";
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/3";
 
     # blueprint.url = "path:/Users/leporuid/Developer/blueprint";
