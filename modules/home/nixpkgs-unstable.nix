52f01129e57963638623f18373f00a589c74357b
Yu-Min Peng <leporuid@icloud.com>
2026-04-13 01:49:34 +0800

    Delete modules directory

--- modules/home/nixpkgs-unstable.nix
+++
@@ -1,8 +0,0 @@
-{ inputs, pkgs, ... }:
-{
-  _module.args.pkgs' = import inputs.nixpkgs-unstable {
-    system = pkgs.stdenv.hostPlatform.system;
-    config.allowUnfree = true;
-    overlays = [ ];
-  };
-}
