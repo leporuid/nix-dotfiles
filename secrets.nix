c57739298cfcef0bbb1d80d9f168909365d66387
Yu-Min Peng <leporuid@icloud.com>
2026-04-26 02:01:18 +0800

    Update secrets.nix

--- secrets.nix
+++ secrets.nix
@@ -1,12 +1,8 @@
 let
   system = builtins.readFile /etc/ssh/ssh_host_ed25519_key.pub;
-  MagiHoHo = builtins.readFile ./hosts/MagiHoHo/id_ed25519.pub;
   MacBook-Pro = builtins.readFile ./hosts/MacBook-Pro/id_ed25519.pub;
-  leporuid-MagiHoHo = builtins.readFile ./hosts/MagiHoHo/users/leporuid/id_ed25519.pub;
   leporuid-MacBook-Pro  = builtins.readFile ./hosts/MacBook-Pro/users/leporuid/id_ed25519.pub;
   leporuid = [
-    MagiHoHo
-    leporuid-MagiHoHo
     leporuid-MacBook-Pro
     MacBook-Pro
   ];
