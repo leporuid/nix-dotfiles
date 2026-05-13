cf75bbe9a579243755b464b021dc9fb3a00bb7ec
Yu-Min Peng <leporuid@icloud.com>
2026-05-14 03:31:49 +0800

    Delete config/swiftly directory

--- config/swiftly/env.sh
+++
@@ -1,6 +0,0 @@
-export SWIFTLY_HOME_DIR="$HOME/.swiftly"
-export SWIFTLY_BIN_DIR="$HOME/.swiftly/bin"
-export SWIFTLY_TOOLCHAINS_DIR="$HOME/Library/Developer/Toolchains"
-if [[ ":$PATH:" != *":$SWIFTLY_BIN_DIR:"* ]]; then
-    export PATH="$SWIFTLY_BIN_DIR:$PATH"
-fi
